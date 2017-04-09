41d48b5c9c050b87641485c0becbc5c0e8242b18
Goyunho <rhdbsgh0629@naver.com>
2017-04-09 17:24:40 +0900

    add comment

--- main.c
+++ main.c
@@ -5,14 +5,13 @@
 
 #define PRINT_PROCESS 1 // 0: 암/복호화 과정 숨김, 1: 암/복호화 과정 출력
 
-#define KEY_SIZE 10
-#define BLOCK 8
+#define KEY_SIZE 10 // 키 크기 지정
+#define BLOCK 8 // 블록 크기 지정
 #define DATA_TYPE int // short, int, long, long long
 
-typedef unsigned char Byte;
-typedef enum {false, true} bool;
+typedef unsigned char Byte; // 1 Byte 크기 정의
 
-DATA_TYPE strbin2keytype(char *strbin) {
+DATA_TYPE strbin2keytype(char *strbin) { // 문자열로 표시한 이진수를 10진수로 변환한다.
     DATA_TYPE dec=0;
     int i;
     int str_size;
@@ -28,7 +27,7 @@ DATA_TYPE strbin2keytype(char *strbin) {
     return dec;
 }
 
-void print_bin(char *str, DATA_TYPE data) {
+void print_bin(char *str, DATA_TYPE data) { // 데이터를 이진수 문자열로 출력한다.
     int i;
 
     printf("%s", str);
@@ -43,10 +42,11 @@ void print_bin(char *str, DATA_TYPE data) {
     printf("\n");
 }
 
+// 왼쪽으로 shift_value만큼 이동하고 넘어가는 부분은 뒤로 이어붙인다.
 int circular_shift(int data, int max_bit, int index, int offset, int shift_value) {
-    int range;
-    int block;
-    int shifted;
+    int range; // 범위지정
+    int block; // 범위만큼의 블록 추출
+    int shifted; // 시프트 연산 후의 데이터
 
     range = (pow(2, offset+index)-1)-(pow(2, index)-1);
     block = data & range;
@@ -58,7 +58,7 @@ int circular_shift(int data, int max_bit, int index, int offset, int shift_value
 }
 
 DATA_TYPE p10(DATA_TYPE key) {
-    int pattern[KEY_SIZE] = {3, 5, 2, 7, 4, 10, 1, 9, 8, 6};
+    int pattern[KEY_SIZE] = {3, 5, 2, 7, 4, 10, 1, 9, 8, 6}; // 패턴에 따라 key의 해당 위치 값이 after_p10에 순서대로 삽입
     DATA_TYPE after_p10 = 0;
     int i, value;
 
@@ -85,6 +85,7 @@ Byte p8(DATA_TYPE key) {
     return after_p8;
 }
 
+// key으로 key1, key2를 생성
 void key_gen(DATA_TYPE key, Byte *key1, Byte *key2) {
 
     key = p10(key);
@@ -151,8 +152,8 @@ Byte s0(Byte data) {
     };
     int row, col;
 
-    row = ((data & 0b10000000)/128)*2 + (data & 0b00010000)/16; // AD
-    col = ((data & 0b01000000)/64)*2 + (data & 0b00100000)/32; // BC
+    row = ((data & 0b10000000)/128)*2 + (data & 0b00010000)/16; // AD 행값
+    col = ((data & 0b01000000)/64)*2 + (data & 0b00100000)/32; // BC 열값
 
     return sbox[row][col];
 }
@@ -166,8 +167,8 @@ Byte s1(Byte data) {
     };
     int row, col;
 
-    row = ((data & 0b1000)/8*2) + (data & 0b0001)/1; // AD
-    col = ((data & 0b0100)/4*2) + (data & 0b0010)/2; // BC
+    row = ((data & 0b1000)/8*2) + (data & 0b0001)/1; // AD 행값
+    col = ((data & 0b0100)/4*2) + (data & 0b0010)/2; // BC 열값
 
     return sbox[row][col];
 }
@@ -186,18 +187,20 @@ Byte p4(Byte data) {
     return after_p4;
 }
 
+// F 실행
 Byte f(Byte right, Byte key) {
     Byte result = 0;
 
-    result = ep(right);
-    // printf("after ep : "); print_bin(result);
-    result = result ^ key;
-    result = (s0(result) << 2) | s1(result);
-    result = p4(result);
+    result = ep(right); // E/P 실행
+    result = result ^ key; // result xor key
+    result = (s0(result) << 2) | s1(result); // S-Box => s0, s1
+    result = p4(result); // p4
 
     return result;
 }
 
+// 암/복호화 알고리즘이 같으므로 하나의 알고리즘으로 통일.
+// key1, key2의 위치만 바꿔주면 된다.
 Byte cryptogram(Byte plain_text, Byte key1, Byte key2) {
     Byte data;
     Byte left4bit, right4bit;
@@ -236,6 +239,7 @@ print_bin("after IP-1 >> ", data);
     return data;
 }
 
+// 암호화 함수
 Byte encrypt(Byte plain_text, DATA_TYPE key) {
     Byte key1, key2;
 
@@ -246,6 +250,7 @@ printf("[encrypt]\n");
     return cryptogram(plain_text, key1, key2);
 }
 
+// 복호화 함수
 Byte decrypt(Byte plain_text, DATA_TYPE key) {
     Byte key1, key2;
 
@@ -257,12 +262,12 @@ printf("[decrypt]\n");
 }
 
 int main(int argc, char *argv[]) {
-    DATA_TYPE key = 151;// 679; // 1010100111
-    Byte plain_text = 165;// 174; // 10101110
+    DATA_TYPE key = 679; // 1010100111, 디폴트 값
+    Byte plain_text = 174; // 10101110, 디폴트 값
     Byte ciphertext = 0;
     Byte deciphertext = 0;
 
-    if(argc > 1) {
+    if(argc > 1) { // 키와 평문의 입력이 있으면 대입
         key = strbin2keytype(argv[1]);
         plain_text = strbin2keytype(argv[2]);
     }
